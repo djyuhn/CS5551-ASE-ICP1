3e123b1144bfa1bd5a5223a2e9c42296de735028
DJ Yuhn <djyuhn@gmail.com>
2018-03-23 22:25:18 -0500

    Documentation
    Include screenshots for wiki documentation

--- ICP10/source/Similar_Movies/server.js
+++ ICP10/source/Similar_Movies/server.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // set port
 var port = process.env.PORT || 8080
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/'));
 
 // routes
 
 app.get("/", function(req, res) {
-    res.render("Home");
+    res.sendFile(path.join(__dirname + "/Home.html"));
 })
 
 // Server must listen for requests and send a response
